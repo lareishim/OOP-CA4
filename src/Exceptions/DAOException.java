e3fabe57b810ae4886a6c9002ddd161983c89ba1
lareishim <laremgmt@gmail.com>
2025-02-12 10:00:17 +0000

    created my file path

---
+++ src/Exceptions/DAOException.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+import java.sql.SQLException;
+
+public class DAOException extends SQLException {
+    public DAOException() {
+        // not used
+    }
+
+    public DAOException(String aMessage) {
+        super(aMessage);
+    }
+}
